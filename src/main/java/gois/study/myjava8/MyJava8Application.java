f346c9fe763fa908cbf5da4591901e0c8d372967
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-14 00:38:59 +0000

    Initial Commit

---
+++ src/main/java/gois/study/myjava8/MyJava8Application.java
@@ -0,0 +1,13 @@
+package gois.study.myjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJava8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJava8Application.class, args);
+    }
+
+}
---
+++ src/test/java/gois/study/myjava8/MyJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package gois.study.myjava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJava8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
